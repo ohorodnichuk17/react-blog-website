8820ad787ee326317b68c6d5b30dde5acdc3e900
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-21 11:00:39 +0300

    feat: complete header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import logo from '../assets/logo.png'
+import { FaTelegram } from "react-icons/fa";
 
 function Header() {
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center mt-3">
             <img src={logo} alt="Logotype" className="w-[180px]"/>
             <ul className="flex gap-4 md:gap-14">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
+                <li className="hover:font-bold cursor-pointer">Home</li>
+                <li className="hover:font-bold cursor-pointer">About</li>
+                <li className="hover:font-bold cursor-pointer">Contact</li>
             </ul>
-            <button>Subscribe</button>
+            <button className="bg-blue-500 rounded-full text-white flex items-center">
+                <FaTelegram className="mr-2 text-[20px]" /> Telegram
+            </button>
         </div>
     );
 }
